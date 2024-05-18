c879dda00cac128af49a3b3186384502b7c526d2
AriamoJ <ojaimesd@gmail.com>
2024-05-17 20:40:13 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AriamoJ.py
@@ -0,0 +1,38 @@
+# https://https://www.python.org/
+
+# Comentario en una linea
+
+"""
+Esto es un 
+comentario 
+en varias lineas
+"""
+
+'''
+Esto tambien es un comentario
+de varias
+lineas
+
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de Mi variable"
+
+#Crear la constatnte en mayusculas para diferenciarla de las variables.
+my_CONSTANT = "Mi Constatente" # Por convencion.
+
+#Datos primitivos
+
+My_int : int = 9
+my_float = 1.5
+my_bool = True
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+Print("Hola, Mundo!")
+
+print(type(My_int))
+print(type(my_float))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
